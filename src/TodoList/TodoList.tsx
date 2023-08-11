2eaf5b18044439417b882f625b2d3c5a0ef0d647
caiyu <6caiyu6@gmail.com>
2023-08-11 15:48:18 +0800

    [chore] change js to ts

---
+++ src/TodoList/TodoList.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const TodoList = () => {
+    return (
+        <div>TO TO </div>
+    )
+}
+
+export default TodoList 
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as ReactDOM from 'react-dom';
+import React from 'react';
+import  TodoList  from './TodoList/TodoList';
+
+ReactDOM.render(<div><TodoList/></div>, document.getElementById('root'))
