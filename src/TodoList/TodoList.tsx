743c127b2674830be8b7d9cfcc0f8f64857f7605
CandiceCaiYu <6caiyu6@gmail.com>
2023-08-17 14:37:38 +0800

    [feature] add to do list function and includes get, post, delete

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -3,11 +3,38 @@ import { axiosRequest } from "../utils/axios/axiosUtils"
 
 const TodoList = () => {
     const [list, setList] = useState([])
+    const [inputText, setInputText] = useState()
+    const handleChange = (e) => {
+        setInputText(e.target?.value)
+    }
+
+    const createBook = async (text:string) => {
+        const result = await axiosRequest({
+            method:'post',
+            url: 'http://localhost:4000/posts',
+            data: {
+                title: text
+            }
+        })
+    }
+
+    const handleAddItem = async () => {
+        await createBook(inputText)
+        retrieveList()
+    }
+
+    const handleDelete = async (id: string) => {
+        await axiosRequest({
+            method: 'delete',
+            url: `http://localhost:4000/posts/${id}`,
+        })
+        retrieveList()
+    }
 
     const retrieveList = async () => {
         const result = await axiosRequest({
             method:'get',
-            url: 'http://localhost:4000/posts'
+            url: 'http://localhost:4000/posts',
         })
         setList(result.data)
     }
@@ -19,9 +46,13 @@ const TodoList = () => {
     return (
         <div>
         <h1>TO DO LIST</h1>
+            <label>
+                <input onChange={handleChange}/>
+                <button onClick={handleAddItem}>ADD</button>
+            </label>
             <ul>
                 {list?.map(item => (
-                    <li>{item.title}</li>
+                    <li onClick={() => handleDelete(item.id)}>{item.title}</li>
                 ))}
             </ul>
         </div>
--- src/mock-api/json-server.js
+++ src/mock-api/json-server.js
@@ -3,13 +3,20 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname,'db.json'))
 const middlewares = jsonServer.defaults()
+const PORT = 4000
 
 server.use(middlewares)
 
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
-    res.sendStatus(200)
+   if(req.method === 'post') {
+       req.body.createAt = Date.now()
+   }
+   next()
 })
 server.use(router)
-server.listen(4000, () => {
-    console.log('JSON Server is running')
+server.listen(PORT, () => {
+    console.log('JSON Server is running, port is'+PORT)
 })
